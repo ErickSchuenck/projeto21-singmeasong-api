9e609d7d2acb5d736424cb90b63837d41e45909e
ErickSchuenck <esfmokdeci@gmail.com>
2022-07-29 21:13:55 -0300

    first commit

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../src/database.js";
+
+async function main(){
+    await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
+
+    await prisma.recommendation.createMany({
+        data: [
+            {
+                name: `UNIQUE random name 1 ${new Date().getTime()}`,
+                youtubeLink: 'https://youtu.be/vik-PASUVuE',
+                score: 8
+            },
+            {
+                name: `UNIQUE random name 2 ${new Date().getTime()}`,
+                youtubeLink: 'https://www.youtube.com/watch?v=27Ufxnmwayo',
+                score: 2
+            },
+            {
+                name: `UNIQUE random name 3 ${new Date().getTime()}`,
+                youtubeLink: 'https://www.youtube.com/watch?v=ckI-Se1NFd4',
+                score: 5
+            },
+            {
+                name: `UNIQUE random name 4 ${new Date().getTime()}`,
+                youtubeLink: 'https://www.youtube.com/watch?v=H15YC7ICsCw',
+                score: 3
+            },
+        ]
+    });
+}
+
+main().catch(e => {
+  console.log(e);
+  process.exit(1);
+}).finally(async () => {
+  await prisma.$disconnect();
+})
---
+++ test/integrationTestsSuite.test.ts
@@ -0,0 +1,61 @@
+import supertest from "supertest";
+import app from "../src/app.js"
+import {prisma} from "../src/database.js"
+
+afterAll(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations;`;
+  await prisma.$disconnect();
+});
+
+describe ("Recommendation test suite", () => {
+  it ("given an name and youtube link, create recommendation", async () => {
+    const createRecomendationInput = {
+      name: `UNIQUE random name ${new Date().getTime()}`,
+      youtubeLink: 'https://www.youtube.com/watch?v=vik-PASUVuE'
+    }
+    const response = await supertest(app).post(`/recommendations/`).send(createRecomendationInput);
+    expect(response.status).toBe(201)
+  });
+
+  it ("given an invalid name and/or invalid youtube link, fail to create recommendation", async () => {
+    const createRecomendationInput = {
+      name: false,
+      youtubeLink: false
+    }
+    const response = await supertest(app).post(`/recommendations/`).send(createRecomendationInput);
+    expect(response.error).not.toBe(null)
+  });
+
+  it ("return all the recommendations", async () => {
+    const response = await supertest(app).get(`/recommendations/`);
+    expect(response.status).toBe(200)
+  });
+
+  it ("return a random recommendation", async () => {
+    const response = await supertest(app).get('/recommendations/random');
+    expect(response.status).toBe(200)
+    expect(response.body.length).not.toBe(null)
+  });
+
+  it ("return the top voted recommendation", async () => {
+    const response = await supertest(app).get(`/recommendations/top/1`);
+    expect(response.status).toBe(200)
+    expect(response.body.length).not.toBe(null)
+  });
+
+  it ("get a recommendation by id", async () => {
+    const response = await supertest(app).get(`/recommendations/1`);
+    expect(response.status).toBe(200)
+    expect(response.body.length).not.toBe(null)
+  });
+
+  // it ("upvote a recommendation by id", async () => {
+  //   const response = await supertest(app).post(`/recommendations/1/upvote`);
+  //   expect(response.status).toBe(200)
+  // });
+
+  // it ("downvote a recommendation by id", async () => {
+  //   const response = await supertest(app).post(`/recommendations/1/downvote`);
+  //   expect(response.status).toBe(200)
+  // });
+});
